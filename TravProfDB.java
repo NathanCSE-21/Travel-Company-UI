907dbbb8bd4d90ddfa723903b08fe56eb3f9c269
Nathan Nguyen <55767560+NathanCSE-21@users.noreply.github.com>
2020-03-20 22:11:06 -0400

    Update TravProfDB.java

--- TravProfDB.java
+++ TravProfDB.java
@@ -1,18 +1,16 @@
 import java.util.*;
 import java.io.*;
 
-public class TravProfDB {
+public class TravProfDB implements Serializable{
     static String filename;
-    static TravProf profile;
-    public static ArrayList<TravProf> travelerList = new ArrayList<>();
-    static boolean exception;
+    static ArrayList<TravProf> travelerList = new ArrayList<TravProf>();
 
     public TravProfDB(String filename){
         this.filename = filename;
     }
 
-    static void insertNewProfile(TravProf newprofile){
-        profile = newprofile;
+    public void insertNewProfile(TravProf newprofile){
+        this.travelerList.add(newprofile);
     }
 
     static void deleteProfile(String agentID, String LastName){
@@ -32,76 +30,76 @@ public class TravProfDB {
     }
 
     // Save Database
-    static void writeAllTravProf(){
-        // add profile into a list.
-        travelerList.add(profile);
+    static void writeAllTravProf() throws IOException{
 
-        // Push into the file name.
-        try {
-            FileOutputStream fos = new FileOutputStream(filename);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
+        // Try to pull data from filename
+        // If filename exists push profile on it
+        // If not create new one and push on it.
+        ArrayList <TravProf> newprofile = new ArrayList<>();
+        boolean exception = false;
 
-            oos.writeObject(travelerList);
-
-            oos.close();
-            fos.close();
-
-            System.out.println("Success");
-        }
-        catch(IOException ioe){
-            // Pull data using method
-            initializeDataBase(true);
-        }
-
-
-
-    }
-
-    // Pull data from filename.
-    static void initializeDataBase(boolean write){
-        ArrayList<TravProf> profiledata = new ArrayList<>();
+        // Pull object from file
         try{
             FileInputStream fis = new FileInputStream(filename);
             ObjectInputStream ois = new ObjectInputStream(fis);
 
-            profiledata = (ArrayList) ois.readObject();
+            newprofile = (ArrayList) ois.readObject();
+
+            ois.close();
+            fis.close();
+        } catch(IOException ioe){
+            // IF you can't pull object then file does not exists
+            // So you push and create a new profile.
+
+            // push new data in file.
+            FileOutputStream fos = new FileOutputStream(filename);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(travelerList);
+            exception = true;
+        }catch(ClassNotFoundException c){
+            System.out.println("Class not found");
+            return;
+        }
+
+        // If file exists, then simply add object to the file.
+        if(!exception){
+            // Push into the file
+            FileOutputStream fos = new FileOutputStream(filename);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(travelerList);
+
+            oos.close();
+            fos.close();
+        }
+    }
+
+    // Pull data from filename.
+    public void initializeDataBase(){
+        try{
+            FileInputStream fis = new FileInputStream(filename);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            travelerList = (ArrayList) ois.readObject();
 
             ois.close();
             fis.close();
 
         }
         catch(IOException ioe){
-            // If file not found.
-            // Check if we need to write(save) to database
-            // If we need to write then create new file and set exception boolean to true.
-            if(write){
-                exception = true;
-                profiledata.add(profile);
-
-                try {
-                    FileOutputStream fos = new FileOutputStream(filename);
-                    ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-                    oos.writeObject(profiledata);
-
-                    oos.close();
-                    fos.close();
-                }
-                catch(IOException io){
-                    System.out.println("Writing to file failed.");
-                    return;
-                }
-            }
-            else {
-                System.out.println("File for this agent is not found. Please create a new profile first");
-                return;
-            }
+            System.out.println("File not found. Please create a new profile and save it.");
+            return;
         }
         catch(ClassNotFoundException c){
             System.out.println("Class not found");
             c.printStackTrace();
             return;
         }
+
+        for(TravProf profile: travelerList){
+            System.out.println(profile);
+        }
+
 
     }
 
